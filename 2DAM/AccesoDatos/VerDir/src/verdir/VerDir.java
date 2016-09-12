e1fa93298c0705877a448bad73beb7beb8b2fc6f
Lucas <lucascervantesc@gmail.com>
2016-09-12 09:31:07 +0200

    metodo ver informacion fichero

--- 2DAM/AccesoDatos/VerDir/src/verdir/VerDir.java
+++ 2DAM/AccesoDatos/VerDir/src/verdir/VerDir.java
@@ -17,7 +17,31 @@ public class VerDir {
         for (int i = 0; i < archivos.length; i++) {
             System.out.println(archivos[i]);
         }
+        
+        verInf();
 
     }
-
+    
+    public static void verInf() {
+        
+        System.out.println("Informacion sobre fichero: ");
+        
+        File f = new File("src\\verDir.java");
+        
+        System.out.println("Nombre de fichero: " + f.getName());
+        System.out.println("Ruta: " + f.getPath());
+        System.out.println("Ruta absoluta: " + f.getAbsolutePath());
+        System.out.println("Se puede escribir: " + f.canWrite());
+        System.out.println("Se puede leer: " + f.canRead());
+        System.out.println("Tamaño: " + f.length());
+        System.out.println("Es un fichero: " + f.isFile());
+    }
+    
+    public static void listarDirectorio(String directorio, int tipoLista) {
+        //tipo lista
+        //tipo columna
+        //tipo tabla
+    }
+    
+    
 }
